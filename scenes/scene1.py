109bbb9fb13163f3293fac511292e3939a23b094
AronMaron <86151090+AronMaron@users.noreply.github.com>
2026-01-08 10:44:44 +0000

    Some learningfor manim and adding rotation

--- scenes/scene1.py
+++ scenes/scene1.py
@@ -1,7 +1,51 @@
 from manim import*
 
-class TestScene(Scene):
+class BaseScene(Scene):
     def construct(self):
-        text = Text("Manim Works!!!")
-        self.play(Write(text))
-        self.wait(2)
+        square = Square()
+        circle = Circle()
+
+        self.play(Create(square))
+        self.wait(0.5)
+        self.play(Transform(square,circle))
+        self.wait(0.5)
+
+class Basics(Scene):
+    def construct(self):
+        axes = Axes()
+        dot =Dot(ORIGIN)
+        square = Square()
+
+        self.play(Create(axes))
+        self.play(FadeIn(dot))
+        self.play(Create(square))
+        self.wait(1)
+
+class LinkTest(Scene):
+    def construct(self):
+        link = Rectangle(width=3,height=0.3)
+        joint = Dot(link.get_left())
+
+        self.add(joint)
+        self.play(Create(link))
+        self.wait(1)
+
+class RotationTest(Scene):
+    def construct(self):
+        link = Rectangle(width=3, height=0.3)
+        joint = Dot(link.get_left())
+
+        self.add(joint)
+        self.add(link)
+
+        self.play(
+            Rotate(
+                link,
+                angle=PI/4,
+                about_point=joint.get_center()
+            )
+        )
+        self.wait(1)
+
+        
+
